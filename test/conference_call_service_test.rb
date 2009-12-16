0034ab2ed2c9ebf7930aea385913736c06a3b6e6
Julian Fischer <jfischer@ravel.start.uni-saarland.de>
2009-12-16 10:39:40 +0100

    tests adapted

--- test/conference_call_service_test.rb
+++ test/conference_call_service_test.rb
@@ -25,7 +25,7 @@ class ConferenceCallServiceTest < Test::Unit::TestCase
     assert( response.conference_ids.size > 0, "There should be at least one conference id." )
 
     # Comment in to see a list of available conferences
-    puts response
+    # puts response
   end
 
   def test_create_conference
@@ -34,61 +34,66 @@ class ConferenceCallServiceTest < Test::Unit::TestCase
     response = @service.create_conference("max.mustermann", conf_details, schedule)
     assert_instance_of(ConferenceCallService::CreateConferenceResponse, response)
     assert_equal("0000", response.error_code)
-    assert_not_nil(response.conference_id)     
+    assert_not_nil(response.conference_id)
   end
 
   def test_commit_conference
-    conf_id = "967191"
-    response = @service.commit_conference(conf_id)
-    assert_instance_of(ConferenceCallService::CommitConferenceResponse, response)
-    assert_equal("0000", response.error_code)
+    with_temporary_conference do |conf_id|
+      response = @service.commit_conference(conf_id)
+      assert_instance_of(ConferenceCallService::CommitConferenceResponse, response)
+      assert_equal("0000", response.error_code)
+    end
   end
 
   def test_get_conference_status
-    conference_id = "967191"
-    response = @service.get_conference_status(conference_id, ConferenceCallService::ConferenceConstants.STATUS_ALL, ServiceEnvironment.MOCK)
+    with_temporary_conference do |conference_id|
+      response = @service.get_conference_status(conference_id, ConferenceCallService::ConferenceConstants.STATUS_ALL, ServiceEnvironment.MOCK)
 
-    assert_instance_of(ConferenceCallService::GetConferenceStatusResponse, response)
-    assert_equal("0000", response.error_code)
+      assert_instance_of(ConferenceCallService::GetConferenceStatusResponse, response)
+      assert_equal("0000", response.error_code)
 
-    assert_instance_of(ConferenceCallService::ConferenceDetails, response.details)
-    assert_not_nil(response.details.name)
-    assert_not_nil(response.details.description)
-    assert_not_nil(response.details.duration)
+      assert_instance_of(ConferenceCallService::ConferenceDetails, response.details)
+      assert_not_nil(response.details.name)
+      assert_not_nil(response.details.description)
+      assert_not_nil(response.details.duration)
 
-    assert_instance_of(ConferenceCallService::ConferenceSchedule, response.schedule)
-    assert_not_nil(response.schedule.minute)
-    assert_not_nil(response.schedule.hour)
-    assert_not_nil(response.schedule.day_of_month)
-    assert_not_nil(response.schedule.month)
-    assert_not_nil(response.schedule.year)
-    assert_not_nil(response.schedule.recurring)
+      assert_instance_of(ConferenceCallService::ConferenceSchedule, response.schedule)
+      assert_not_nil(response.schedule.minute)
+      assert_not_nil(response.schedule.hour)
+      assert_not_nil(response.schedule.day_of_month)
+      assert_not_nil(response.schedule.month)
+      assert_not_nil(response.schedule.year)
+      assert_not_nil(response.schedule.recurring)
+    end
   end
 
   def test_new_participant
-    conference_id = "967191"
-    participant = ConferenceCallService::ParticipantDetails.new('lucas', 'pinto', '+493200000001', 'luc@spin.to', 0)
-    response = @service.new_participant(conference_id, participant)
+    with_temporary_conference do |conference_id|
+      participant = ConferenceCallService::ParticipantDetails.new('lucas', 'pinto', '+493200000001', 'luc@spin.to', 0)
+      response = @service.new_participant(conference_id, participant)
 
-    assert_instance_of(ConferenceCallService::NewParticipantResponse, response)
-    assert_equal("0000", response.error_code)
-    assert_not_nil(response.participant_id)
+      assert_instance_of(ConferenceCallService::NewParticipantResponse, response)
+      assert_equal("0000", response.error_code)
+      assert_not_nil(response.participant_id)
+    end
   end
 
 
   def test_remove_participant
-    conference_id   = "967191"
-    participant_id  = "abc"
+    with_temporary_conference do |conference_id|
+      participant_id = "abc"
 
-    response = @service.remove_participant(conference_id, participant_id)
-    assert_equal("0000", response.error_code)    
+      response = @service.remove_participant(conference_id, participant_id)
+      assert_equal("0000", response.error_code)
+    end
   end
 
   def test_get_running_conference
-    conf_id = "967191"
-    response = @service.get_running_conference(conf_id)
-    assert_instance_of(ConferenceCallService::GetRunningConferenceResponse, response)
-    assert_equal("0000", response.error_code)
+    with_temporary_conference do |conf_id|
+      response = @service.get_running_conference(conf_id)
+      assert_instance_of(ConferenceCallService::GetRunningConferenceResponse, response)
+      assert_equal("0000", response.error_code)
+    end
   end
 
   def test_get_conference_template_list
@@ -100,9 +105,30 @@ class ConferenceCallServiceTest < Test::Unit::TestCase
   end
 
   def test_remove_conference
-    conf_id = "967191"
+    conf_id = create_conference
     response = @service.remove_conference(conf_id)
     assert_instance_of(ConferenceCallService::RemoveConferenceResponse, response)
     assert_equal("0000", response.error_code)
+  end
+
+  protected
+
+  # Creates a conference and returns its conference id.
+  def create_conference
+    conf_details = ConferenceCallService::ConferenceDetails.new("A very important conf", "A very impressive description", 30)
+    schedule = ConferenceCallService::ConferenceSchedule.new
+    response = @service.create_conference("max.mustermann", conf_details, schedule)
+    return response.conference_id
+  end
+
+  def remove_conference(conference_id)
+    response = @service.remove_conference(conference_id)
+  end
+
+  # Creates a conference, executes the given block and deleted the conf afterwards
+  def with_temporary_conference(&block)
+    conf_id = create_conference
+    yield(conf_id)
+    remove_conference(conf_id)
   end
 end
